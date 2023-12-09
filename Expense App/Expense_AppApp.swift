d0a4417e3419f52f34809f91e6651a1d50e3cac5
John Kramer <hkur.gobimbel@gmail.com>
2023-12-09 10:57:57 +0700

    Initial Commit

---
+++ Expense App/Expense_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_AppApp.swift
+//  Expense App
+//
+//  Created by Mac on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
